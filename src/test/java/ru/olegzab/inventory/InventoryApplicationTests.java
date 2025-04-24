97b63609b742cb926bc502d18f5f8b18b38c0ca8
OlegZabolotnykh <beyondswamps@gmail.com>
2025-04-24 13:10:33 +0300

    Initial commit

---
+++ src/test/java/ru/olegzab/inventory/InventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.olegzab.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
